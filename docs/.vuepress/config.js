41d5169223b2d964449fbbe58df39aef8e8a6fc7
huguanData <contact@craftact.net>
2020-08-01 12:22:25 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  base: '/dmx-tool-help/',
+  // base: '/dmx-tool-help/',
   title: '这里是网页标题',
   description: '这里是网页描述',
   themeConfig: {
@@ -14,7 +14,7 @@ module.exports = {
     ],
     sidebarDepth: 2,
     // 以下是github设置
-    repo: 'huguanData/dmx-tool-help',
+    repo: 'huguanData/huguanData.github.iop',
     repoLabel: 'github',
     // 以下为可选的编辑链接选项
     // 假如你的文档仓库和项目本身不在一个仓库：
@@ -22,7 +22,7 @@ module.exports = {
     // 假如文档不是放在仓库的根目录下：
     docsDir: 'docs',
     // 假如文档放在一个特定的分支下：
-    docsBranch: 'gh-pages',
+    docsBranch: 'master',
     // 默认是 false, 设置为 true 来启用
     editLinks: true,
     // 默认为 "Edit this page"
